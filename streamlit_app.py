8b2b708b34dffcd35194b0401a5594e6c5da44f4
chiarawj <143527620+chiarawj@users.noreply.github.com>
2023-09-01 14:03:21 +0100

    Update streamlit_app.py
    change title and button label

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ try:
 except URLError as e:
   streamlit.error()
 
-streamlit.write('The user entered', fruit_choice)
+#streamlit.write('The user entered', fruit_choice)
 
 #import requests python package library
 #import requests
@@ -60,7 +60,7 @@ streamlit.write('The user entered', fruit_choice)
 #import snowflake.connector
 
 #query metadata Make it look nicer
-streamlit.header("The fruit load list contains:")
+streamlit.header("View Our Fruit List -Add Your Favorites!")
 #Snowflake-related functions
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
@@ -68,7 +68,7 @@ def get_fruit_load_list():
         return my_cur.fetchall()
 
 #add a button to load the fruit
-if streamlit.button('Get Fruit Load List'):
+if streamlit.button('Get Fruit List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
     my_cnx.close()
